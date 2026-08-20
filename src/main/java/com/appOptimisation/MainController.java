0cb081614a14d306fe3217d500c1d46e0c53aa76
Andres-CorderoCamargo <andres.corderocamargo@gmail.com>
2026-08-20 13:50:58 +0200

    Ajout de Button library

--- src/main/java/com/appOptimisation/MainController.java
+++ src/main/java/com/appOptimisation/MainController.java
@@ -3,6 +3,7 @@ package com.appOptimisation;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import oshi.SystemInfo;
